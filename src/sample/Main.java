5cfbd3380f66a93ec9614e69f0b20be174c380fb
Ebbe Vang <ev@evang.dk>
2020-04-13 23:53:13 +0200

    added details to view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 275));
+        primaryStage.setTitle("The MVC Bar");
+         primaryStage.setScene(new Scene(root, 800, 475));
         primaryStage.show();
     }
 
