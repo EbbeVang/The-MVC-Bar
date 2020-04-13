9c656fe19c71b5941eb6c422028c301ef82db44c
Ebbe Vang <ev@evang.dk>
2020-04-14 00:58:57 +0200

    adds observable list

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -34,6 +35,12 @@ public class Controller {
 
     @FXML
     void serveDrink(ActionEvent event) {
-        drinks.remove(0);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        Drink drink = drinks.remove(0);
+        alert.setTitle("Hey - Your drink is ready!");
+        alert.setHeaderText(drink.getName());
+        alert.setContentText(drink.getIngredients());
+        alert.show();
+
     }
 }
