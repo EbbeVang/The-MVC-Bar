741cc60165919d9fe438ab570cba67c061745181
Ebbe Vang <ev@evang.dk>
2020-04-14 00:23:16 +0200

    adds controller content

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TableView<Drink> tableView;
+
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private TextField textFieldIngredients;
+
+    @FXML
+    void orderDrink(ActionEvent event) {
+        Drink drink = new Drink(textFieldName.getText(), textFieldIngredients.getText());
+        tableView.getItems().add(drink);
+    }
 }
