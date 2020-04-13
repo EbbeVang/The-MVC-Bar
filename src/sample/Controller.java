281581f6bad76e629c72c95baddc34a435eed5eb
Ebbe Vang <ev@evang.dk>
2020-04-14 00:42:06 +0200

    adds controller content

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,20 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class Controller {
+
+    ObservableList<Drink> drinks = FXCollections.observableArrayList();
+
+    public void initialize() {
+        // set drinks as table items
+        tableView.setItems(drinks);
+    }
 
     @FXML
     private TableView<Drink> tableView;
@@ -20,6 +28,12 @@ public class Controller {
     @FXML
     void orderDrink(ActionEvent event) {
         Drink drink = new Drink(textFieldName.getText(), textFieldIngredients.getText());
-        tableView.getItems().add(drink);
+        //tableView.getItems().add(drink);
+        drinks.add(drink);
+    }
+
+    @FXML
+    void serveDrink(ActionEvent event) {
+        drinks.remove(0);
     }
 }
